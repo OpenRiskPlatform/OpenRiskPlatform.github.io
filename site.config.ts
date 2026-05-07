3c3c2b3c283f2f8a4c137a5dd3f3f32e797bb645
Illia Chaban <ic-it@mail.com>
2026-05-07 22:00:58 +0200

    upd

--- site.config.ts
+++ site.config.ts
@@ -1,7 +1,8 @@
 export const SITE = "https://openriskplatform.github.io";
-export const BASE_PATH = "/openrisk";
+export const BASE_PATH = "";
 
 export const SDK_URL = "https://openriskplatform.github.io/plugin-sdk";
-export const DISCOVERY_URL = "https://openriskplatform.github.io/plugins-discovery";
+export const DISCOVERY_URL =
+  "https://openriskplatform.github.io/plugins-discovery";
 export const GITHUB_URL = "https://github.com/OpenRiskPlatform";
 export const APP_GITHUB_URL = "https://github.com/OpenRiskPlatform/OpenRisk";
