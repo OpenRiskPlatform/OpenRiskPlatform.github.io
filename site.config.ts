1cc46286c134777bdb428a9687039187e36d0e7c
Illia Chaban <ic-it@mail.com>
2026-05-07 21:37:36 +0200

    first commit

---
+++ site.config.ts
@@ -0,0 +1,7 @@
+export const SITE = "https://openriskplatform.github.io";
+export const BASE_PATH = "/openrisk";
+
+export const SDK_URL = "https://openriskplatform.github.io/plugin-sdk";
+export const DISCOVERY_URL = "https://openriskplatform.github.io/plugins-discovery";
+export const GITHUB_URL = "https://github.com/OpenRiskPlatform";
+export const APP_GITHUB_URL = "https://github.com/OpenRiskPlatform/OpenRisk";
